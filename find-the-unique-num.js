f6e54ca18540816e839830a746b4f85d64106608
Anna Galkevich <emikot321@gmail.com>
2023-08-07 20:28:25 +0300

    2 tasks

---
+++ find-the-unique-num.js
@@ -0,0 +1,9 @@
+// There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+// findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+// findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+// It’s guaranteed that array contains at least 3 numbers.
+
+function findUniq(array) {
+  return array.find((item) => array.indexOf(item) === array.lastIndexOf(item))
+  }
